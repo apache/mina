f23f6333a5de722e2dd9271e77dd3145cab7436b
Trustin Lee <trustin@apache.org>
2005-02-24 02:07:38 +0000

    Fixed: Infinite loop in NettyDecoder
    git-svn-id: https://svn.apache.org/repos/asf/incubator/directory/network/mina/trunk@155136 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/mina/protocol/codec/NettyDecoder.java
+++ src/java/org/apache/mina/protocol/codec/NettyDecoder.java
@@ -104,6 +104,7 @@ public class NettyDecoder implements ProtocolDecoder
                         else
                         {
                             readBuf.clear();
+			    break;
                         }
                     }
                 }
