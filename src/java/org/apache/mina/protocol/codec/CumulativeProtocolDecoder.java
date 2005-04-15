8447fa55ca01f3c218898bfa78e5c89f0d40c93e
Trustin Lee <trustin@apache.org>
2005-04-15 09:13:10 +0000

    * Added CumulativeProtocolDecoder and its test. * Added some JavaDoc and copyright statements.
    git-svn-id: https://svn.apache.org/repos/asf/directory/network/trunk@161424 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/mina/protocol/codec/CumulativeProtocolDecoder.java
@@ -0,0 +1,142 @@
+/*
+ *   @(#) $Id$
+ *
+ *   Copyright 2004 The Apache Software Foundation
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+package org.apache.mina.protocol.codec;
+
+import org.apache.mina.common.ByteBuffer;
+import org.apache.mina.protocol.ProtocolDecoder;
+import org.apache.mina.protocol.ProtocolDecoderOutput;
+import org.apache.mina.protocol.ProtocolSession;
+import org.apache.mina.protocol.ProtocolViolationException;
+
+/**
+ * A {@link ProtocolDecoder} that cumulates the content of received
+ * buffers to a <em>cumulative buffer</em> to help users implement decoders.
+ * <p>
+ * If the received {@link ByteBuffer} is only a part of a message.
+ * decoders should cumulate received buffers to make a message complete or
+ * to postpone decoding until more buffers arrive.
+ * <p>
+ * Here is an example decoder that decodes a list of integers:
+ * <pre>
+ * public class IntegerDecoder extends CumulativeProtocolDecoder {
+ * 
+ *     public IntegerDecoder() {
+ *         super(4);
+ *     }
+ * 
+ *     protected boolean doDecode(ProtocolSession session, ByteBuffer in,
+ *                                ProtocolDecoderOutput out) throws ProtocolViolationException {
+ *         if (in.remaining() < 4) {
+ *             return false; // Cumulate remainder to decode later.
+ *         }
+ *         
+ *         out.write(new Integer(in.getInt()));
+ * 
+ *         // Decoded one integer; CumulativeProtocolDecoder will call me again,
+ *         // so I can decode as many integers as possible.
+ *         return true;
+ *     }
+ * }
+ * </pre>
+ * 
+ * @author Trustin Lee (trustin@apache.org)
+ * @version $Rev$, $Date$
+ */
+public abstract class CumulativeProtocolDecoder implements ProtocolDecoder {
+    
+    /** Cumulation buffer */
+    private ByteBuffer buf;
+    
+    /**
+     * Creates a new instance with the specified default capacity of
+     * cumulative buffer.  Please note that the capacity increases
+     * automatically.
+     */
+    protected CumulativeProtocolDecoder( int defaultCapacity )
+    {
+        buf = ByteBuffer.allocate( defaultCapacity );
+    }
+    
+    /**
+     * Cumulates content of <tt>in</tt> into internal buffer and forwards
+     * decoding request to {@link #doDecode(ProtocolSession, ByteBuffer, ProtocolDecoderOutput)}.
+     * <tt>doDecode()</tt> is invoked repeatedly until it returns <tt>false</tt>
+     * and the cumulative buffer is compacted after decoding ends.
+     * 
+     * @throws IllegalStateException if your <tt>doDecode()</tt> returned
+     *                               <tt>true</tt> not consuming the cumulative buffer.
+     */
+    public void decode( ProtocolSession session, ByteBuffer in,
+                        ProtocolDecoderOutput out ) throws ProtocolViolationException
+    {
+        put( in );
+        
+        ByteBuffer buf = this.buf;
+        buf.flip();
+
+        for( ;; )
+        {
+            int oldPos = buf.position();
+            if( !doDecode( session, buf, out ) )
+            {
+                break;
+            }
+            
+            if( buf.position() == oldPos )
+            {
+                throw new IllegalStateException(
+                        "doDecode() can't return true when buffer is not consumed." );
+            }
+        }
+
+        buf.compact();
+    }
+    
+    /**
+     * Implement this method to consume the specified cumulative buffer and
+     * decode its content into message(s). 
+     *  
+     * @param in the cumulative buffer
+     * @return <tt>true</tt> if and only if there's more to decode in the buffer
+     *         and you want to have <tt>doDecode</tt> method invoked again.
+     * @throws ProtocolViolationException if cannot decode <tt>in</tt>.
+     */
+    protected abstract boolean doDecode( ProtocolSession session, ByteBuffer in,
+                                         ProtocolDecoderOutput out ) throws ProtocolViolationException;
+
+
+    private void put( ByteBuffer in )
+    {
+        ByteBuffer buf = this.buf;
+        
+        // Check capacity
+        if( in.remaining() > buf.remaining() ) {
+            int newCapacity = buf.position() + in.remaining();
+            ByteBuffer newBuf = ByteBuffer.allocate( newCapacity );
+            buf.flip();
+            newBuf.put( buf );
+            buf.release(); // Release old buffer
+            buf = newBuf;
+        }
+        
+        // Copy to cumulative buffer
+        buf.put( in.buf() );
+        this.buf = buf;
+    }
+}
---
+++ src/test/org/apache/mina/protocol/codec/CumulativeProtocolDecoderTest.java
@@ -0,0 +1,165 @@
+/*
+ *   @(#) $Id$
+ *
+ *   Copyright 2004 The Apache Software Foundation
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+package org.apache.mina.protocol.codec;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import org.apache.mina.common.ByteBuffer;
+import org.apache.mina.protocol.ProtocolDecoderOutput;
+import org.apache.mina.protocol.ProtocolSession;
+import org.apache.mina.protocol.ProtocolViolationException;
+
+/**
+ * Tests {@link CumulativeProtocolDecoder}.
+ * 
+ * @author Trustin Lee (trustin@apache.org)
+ * @version $Rev$, $Date$ 
+ */
+public class CumulativeProtocolDecoderTest extends TestCase
+{
+    private ByteBuffer buf;
+    private IntegerDecoder decoder;
+    private IntegerDecoderOutput output;
+
+    public static void main(String[] args)
+    {
+        junit.textui.TestRunner.run(CumulativeProtocolDecoderTest.class);
+    }
+
+    protected void setUp() throws Exception
+    {
+        buf = ByteBuffer.allocate( 16 );
+        decoder = new IntegerDecoder();
+        output = new IntegerDecoderOutput();
+    }
+
+    protected void tearDown() throws Exception
+    {
+    }
+    
+    public void testCumulation() throws Exception
+    {
+        buf.put( (byte) 0 );
+        buf.flip();
+        
+        decoder.decode( null, buf, output );
+        Assert.assertEquals( 0, output.getValues().size() );
+        Assert.assertEquals( buf.limit(), buf.position() );
+        
+        buf.clear();
+        buf.put( (byte) 0 );
+        buf.put( (byte) 0 );
+        buf.put( (byte) 1 );
+        buf.flip();
+
+        decoder.decode( null, buf, output );
+        Assert.assertEquals( 1, output.getValues().size() );
+        Assert.assertEquals( new Integer( 1 ), output.getValues().get( 0 ) );
+        Assert.assertEquals( buf.limit(), buf.position() );
+    }
+    
+    public void testRepeatitiveDecode() throws Exception
+    {
+        for( int i = 0; i < 4; i ++ )
+        {
+            buf.putInt( i );
+        }
+        buf.flip();
+        
+        decoder.decode( null, buf, output );
+        Assert.assertEquals( 4, output.getValues().size() );
+        Assert.assertEquals( buf.limit(), buf.position() );
+        
+        List expected = new ArrayList();
+        for( int i = 0; i < 4; i ++ )
+        {
+            expected.add( new Integer( i ) );
+        }
+        Assert.assertEquals( expected, output.getValues() );
+        
+    }
+    
+    public void testWrongImplementationDetection() throws Exception {
+        try
+        {
+            new WrongDecoder().decode( null, buf, output );
+            Assert.fail();
+        }
+        catch( IllegalStateException e )
+        {
+            // OK
+        }
+    }
+    
+    private static class IntegerDecoder extends CumulativeProtocolDecoder
+    {
+        protected IntegerDecoder()
+        {
+            super( 4 );
+        }
+
+        protected boolean doDecode( ProtocolSession session, ByteBuffer in,
+                                    ProtocolDecoderOutput out ) throws ProtocolViolationException
+        {
+            if( in.remaining() < 4 )
+                return false;
+            
+            out.write( new Integer( in.getInt() ) );
+            return true;
+        }
+        
+    }
+    
+    private static class IntegerDecoderOutput implements ProtocolDecoderOutput
+    {
+        private List values = new ArrayList();
+
+        public void write( Object message )
+        {
+            values.add( message );
+        }
+        
+        public List getValues()
+        {
+            return values;
+        }
+        
+        public void clear()
+        {
+            values.clear();
+        }
+    }
+    
+    private static class WrongDecoder extends CumulativeProtocolDecoder
+    {
+        public WrongDecoder()
+        {
+            super( 4 );
+        }
+
+        protected boolean doDecode( ProtocolSession session, ByteBuffer in,
+                                    ProtocolDecoderOutput out ) throws ProtocolViolationException {
+            return true;
+        }
+    }
+}
