b9828cde753a7834d4da7b5582612942ec6ae0e9
Trustin Lee <trustin@apache.org>
2005-04-15 09:13:10 +0000

    * Added CumulativeProtocolDecoder and its test. * Added some JavaDoc and copyright statements.
    git-svn-id: https://svn.apache.org/repos/asf/directory/network/trunk@161424 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/mina/common/ByteBufferTest.java
+++ src/test/org/apache/mina/common/ByteBufferTest.java
@@ -1,5 +1,20 @@
 /*
- * @(#) $Id$
+ *   @(#) $Id$
+ *
+ *   Copyright 2004 The Apache Software Foundation
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
  */
 package org.apache.mina.common;
 
@@ -7,10 +22,10 @@ import junit.framework.Assert;
 import junit.framework.TestCase;
 
 /**
- * TODO Document me.
+ * Tests {@link ByteBuffer}.
  * 
  * @author Trustin Lee (trustin@apache.org)
- * @version $Rev$, $Date$, 
+ * @version $Rev$, $Date$ 
  */
 public class ByteBufferTest extends TestCase
 {
--- src/test/org/apache/mina/util/IoHandlerFilterImpl.java
+++ src/test/org/apache/mina/util/IoHandlerFilterImpl.java
@@ -1,12 +1,30 @@
 /*
- * @(#) $Id$
+ *   @(#) $Id$
+ *
+ *   Copyright 2004 The Apache Software Foundation
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
  */
 package org.apache.mina.util;
 
+import org.apache.mina.io.IoHandlerFilter;
 import org.apache.mina.io.IoHandlerFilterAdapter;
+import org.apache.mina.io.IoHandlerFilterChain;
 
 /**
- * TODO Document me.
+ * Bogus implementation of {@link IoHandlerFilter} to test
+ * {@link IoHandlerFilterChain}.
  * 
  * @author Trustin Lee (trustin@apache.org)
  * @version $Rev$, $Date$
