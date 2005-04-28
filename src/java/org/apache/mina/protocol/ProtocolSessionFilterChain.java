05bf6afd4e816ac93d104c4c188d581d9025534a
Trustin Lee <trustin@apache.org>
2005-04-28 11:28:41 +0000

    * Added some JavaDocs to newly added code
    git-svn-id: https://svn.apache.org/repos/asf/directory/network/trunk@165129 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/mina/protocol/ProtocolSessionFilterChain.java
+++ src/java/org/apache/mina/protocol/ProtocolSessionFilterChain.java
@@ -1,19 +1,48 @@
+/*
+ *   @(#) $Id$
+ *
+ *   Copyright 2004 The Apache Software Foundation
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
 package org.apache.mina.protocol;
 
+import org.apache.mina.io.IoHandlerFilterChain;
+import org.apache.mina.io.IoSession;
+import org.apache.mina.io.IoSessionManagerFilterChain;
+
 /**
- * TODO document me
+ * An {@link IoHandlerFilterChain} that forwards <tt>filterWrite</tt>
+ * requests to the specified {@link IoSessionManagerFilterChain}.
+ * <p>
+ * This filter chain is used by implementations of {@link IoSession}s.
+ * 
+ * @author The Apache Directory Project (dev@directory.apache.org)
+ * @author Trustin Lee (trustin@apache.org)
+ * @version $Rev$, $Date$
  */
 public class ProtocolSessionFilterChain extends AbstractProtocolHandlerFilterChain {
 
-    private final ProtocolSessionManagerFilterChain prevChain;
+    private final ProtocolSessionManagerFilterChain managerChain;
 
-    public ProtocolSessionFilterChain( ProtocolSessionManagerFilterChain prevChain )
+    public ProtocolSessionFilterChain( ProtocolSessionManagerFilterChain managerChain )
     {
-        this.prevChain = prevChain;
+        this.managerChain = managerChain;
     }
 
     protected void doWrite( ProtocolSession session, Object message )
     {
-        prevChain.filterWrite( session, message );
+        managerChain.filterWrite( session, message );
     }
 }
--- src/java/org/apache/mina/protocol/ProtocolSessionManagerFilterChain.java
+++ src/java/org/apache/mina/protocol/ProtocolSessionManagerFilterChain.java
@@ -1,11 +1,35 @@
+/*
+ *   @(#) $Id$
+ *
+ *   Copyright 2004 The Apache Software Foundation
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
 package org.apache.mina.protocol;
 
 import org.apache.mina.common.IdleStatus;
 
 /**
- * TODO document me
+ * An {@link ProtocolHandlerFilterChain} that forwards all events
+ * except <tt>filterWrite</tt> to the {@link ProtocolSessionFilterChain}
+ * of the recipient session.
+ * <p>
+ * This filter chain is used by implementations of {@link ProtocolSessionManager}s.
  * 
- * @author The Apache Directory Project
+ * @author The Apache Directory Project (dev@directory.apache.org)
+ * @author Trustin Lee (trustin@apache.org)
+ * @version $Rev$, $Date$
  */
 public abstract class ProtocolSessionManagerFilterChain extends AbstractProtocolHandlerFilterChain {
 
