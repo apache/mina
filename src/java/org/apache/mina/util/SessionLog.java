b4ebfd2e8fe815907f7304daee03f9a0849189bf
Trustin Lee <trustin@apache.org>
2005-07-01 05:09:58 +0000

    Fix for DIRMINA-65 * Modified SessionManagerFilterChain implementations to wake up selectors only when write request queue is empty.
    Fix for DIRMINA-71
    * Added prefix to log message.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/directory/network/branches/0.7@208715 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/mina/util/SessionLog.java
+++ src/java/org/apache/mina/util/SessionLog.java
@@ -1,3 +1,21 @@
+/*
+ *   @(#) $Id$
+ *
+ *   Copyright 2004 The Apache Software Foundation
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
 package org.apache.mina.util;
 
 import java.util.logging.Level;
@@ -63,7 +81,7 @@ public class SessionLog {
         Logger log = getLogger( session );
         if( log.isLoggable( level ) )
         {
-            log.log( level, message );
+            log.log( level, String.valueOf( session.getAttribute( PREFIX ) ) + message );
         }
     }
 
@@ -72,7 +90,7 @@ public class SessionLog {
         Logger log = getLogger( session );
         if( log.isLoggable( level ) )
         {
-            log.log( level, message, cause );
+            log.log( level, String.valueOf( session.getAttribute( PREFIX ) ) + message, cause );
         }
     }
 }
