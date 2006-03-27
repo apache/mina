42baa8399c8cfcfbc481bb0ca71e1e8cd3d641c7
Trustin Lee <trustin@apache.org>
2006-03-27 07:34:16 +0000

    Added SynchronizedProtocolEncoder and SynchronizedProtocolDecoder
    git-svn-id: https://svn.eu.apache.org/repos/asf/directory/trunks/mina@389039 13f79535-47bb-0310-9956-ffa450edef68

---
+++ core/src/main/java/org/apache/mina/filter/codec/SynchronizedProtocolDecoder.java
@@ -0,0 +1,72 @@
+/*
+ *   @(#) $Id$
+ *
+ *   Copyright 2004 The Apache Software Foundation
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+package org.apache.mina.filter.codec;
+
+import org.apache.mina.common.ByteBuffer;
+import org.apache.mina.common.IoSession;
+
+/**
+ * A {@link ProtocolDecoder} implementation which decorates an existing decoder
+ * to be thread-safe.  Please be careful if you're going to use this decorator
+ * because it can be a root of performance degradation in a multi-thread
+ * environment.
+ * 
+ * @author The Apache Directory Project (dev@directory.apache.org)
+ * @version $Rev$, $Date$
+ */
+public class SynchronizedProtocolDecoder implements ProtocolDecoder
+{
+    private final ProtocolDecoder decoder;
+
+    /**
+     * Creates a new instance which decorates the specified <tt>decoder</tt>.
+     */
+    public SynchronizedProtocolDecoder( ProtocolDecoder decoder )
+    {
+        if( decoder == null )
+        {
+            throw new NullPointerException( "decoder" );
+        }
+        this.decoder = decoder;
+    }
+    
+    /**
+     * Returns the decoder this decoder is decorating.
+     */
+    public ProtocolDecoder getDecoder()
+    {
+        return decoder;
+    }
+
+    public void decode( IoSession session, ByteBuffer in, ProtocolDecoderOutput out ) throws Exception
+    {
+        synchronized( decoder )
+        {
+            decoder.decode( session, in, out );
+        }
+    }
+
+    public void dispose( IoSession session ) throws Exception
+    {
+        synchronized( decoder )
+        {
+            decoder.dispose( session );
+        }
+    }
+}
---
+++ core/src/main/java/org/apache/mina/filter/codec/SynchronizedProtocolEncoder.java
@@ -0,0 +1,71 @@
+/*
+ *   @(#) $Id$
+ *
+ *   Copyright 2004 The Apache Software Foundation
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+package org.apache.mina.filter.codec;
+
+import org.apache.mina.common.IoSession;
+
+/**
+ * A {@link ProtocolEncoder} implementation which decorates an existing encoder
+ * to be thread-safe.  Please be careful if you're going to use this decorator
+ * because it can be a root of performance degradation in a multi-thread
+ * environment.
+ * 
+ * @author The Apache Directory Project (dev@directory.apache.org)
+ * @version $Rev$, $Date$
+ */
+public class SynchronizedProtocolEncoder implements ProtocolEncoder
+{
+    private final ProtocolEncoder encoder;
+
+    /**
+     * Creates a new instance which decorates the specified <tt>encoder</tt>.
+     */
+    public SynchronizedProtocolEncoder( ProtocolEncoder encoder )
+    {
+        if( encoder == null )
+        {
+            throw new NullPointerException( "encoder" );
+        }
+        this.encoder = encoder;
+    }
+    
+    /**
+     * Returns the encoder this encoder is decorating.
+     */
+    public ProtocolEncoder getEncoder()
+    {
+        return encoder;
+    }
+
+    public void encode( IoSession session, Object message, ProtocolEncoderOutput out ) throws Exception
+    {
+        synchronized( encoder )
+        {
+            encoder.encode( session, message, out );
+        }
+    }
+
+    public void dispose( IoSession session ) throws Exception
+    {
+        synchronized( encoder )
+        {
+            encoder.dispose( session );
+        }
+    }
+}
