64db42a11e4ac920ebc1c3042fb677e91d9c3856
Trustin Lee <trustin@apache.org>
2006-08-11 10:18:13 +0000

    Created a branch to convert mina-0.8 to Maven 2
    git-svn-id: https://svn.apache.org/repos/asf/directory/sandbox/trustin/mina-0.8-mvnize@430750 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/mina/common/ByteBufferTest.java
+++ src/test/org/apache/mina/common/ByteBufferTest.java
@@ -147,6 +147,26 @@ public class ByteBufferTest extends TestCase
         Assert.assertEquals( 4, buf.capacity() );
     }
     
+    public void testAutoExpandMark() throws Exception
+    {
+    	ByteBuffer buf = ByteBuffer.allocate( 4 ).setAutoExpand( true );
+
+    	buf.put( ( byte ) 0 );
+    	buf.put( ( byte ) 0 );
+    	buf.put( ( byte ) 0 );
+
+    	// Position should be 3 when we reset this buffer.
+    	buf.mark();
+
+    	// Overflow it
+    	buf.put( ( byte ) 0 );
+    	buf.put( ( byte ) 0 );
+    	
+    	Assert.assertEquals( 5, buf.position() );
+    	buf.reset();
+    	Assert.assertEquals( 3, buf.position() );
+    }
+    
     public void testPooledProperty() throws Exception
     {
         ByteBuffer buf = ByteBuffer.allocate( 16 );
