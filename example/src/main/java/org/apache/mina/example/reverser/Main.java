37cb2662d10cb3b1b4a78904b3dbf9ca92aa25a2
Trustin Lee <trustin@apache.org>
2006-09-20 06:22:42 +0000

    Applied the best practice of adding filters
    git-svn-id: https://svn.apache.org/repos/asf/directory/trunks/mina@448088 13f79535-47bb-0310-9956-ffa450edef68

--- example/src/main/java/org/apache/mina/example/reverser/Main.java
+++ example/src/main/java/org/apache/mina/example/reverser/Main.java
@@ -20,9 +20,14 @@
 package org.apache.mina.example.reverser;
 
 import java.net.InetSocketAddress;
+import java.nio.charset.Charset;
 
 import org.apache.mina.common.IoAcceptor;
+import org.apache.mina.filter.LoggingFilter;
+import org.apache.mina.filter.codec.ProtocolCodecFilter;
+import org.apache.mina.filter.codec.textline.TextLineCodecFactory;
 import org.apache.mina.transport.socket.nio.SocketAcceptor;
+import org.apache.mina.transport.socket.nio.SocketAcceptorConfig;
 
 /**
  * (<b>Entry point</b>) Reverser server which reverses all text lines from
@@ -39,10 +44,19 @@ public class Main
     {
         IoAcceptor acceptor = new SocketAcceptor();
 
+        // Prepare the configuration
+        SocketAcceptorConfig cfg = new SocketAcceptorConfig();
+        cfg.setReuseAddress( true );
+        cfg.getFilterChain().addLast( "logger", new LoggingFilter() );
+        cfg.getFilterChain().addLast(
+                "codec",
+                new ProtocolCodecFilter(
+                        new TextLineCodecFactory( Charset.forName( "UTF-8" ) ) ) );
+
         // Bind
         acceptor.bind(
                 new InetSocketAddress( PORT ),
-                new ReverseProtocolHandler() );
+                new ReverseProtocolHandler(), cfg );
 
         System.out.println( "Listening on port " + PORT );
     }
