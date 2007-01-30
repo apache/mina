b1f3115a64a56ce1a88e27c3155958a31888f1fe
Trustin Lee <trustin@apache.org>
2007-01-30 07:14:38 +0000

    Fixed failing tests by delaying event propagation until two sessions finish registeration in IoServiceListenerSupport
    git-svn-id: https://svn.apache.org/repos/asf/mina/branches/1.0@501330 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/mina/transport/vmpipe/support/VmPipeFilterChain.java
+++ core/src/main/java/org/apache/mina/transport/vmpipe/support/VmPipeFilterChain.java
@@ -35,16 +35,26 @@ import edu.emory.mathcs.backport.java.util.concurrent.ConcurrentLinkedQueue;
 public class VmPipeFilterChain extends AbstractIoFilterChain {
 
     private final Queue eventQueue = new ConcurrentLinkedQueue();
+    private boolean flushEnabled;
     
     public VmPipeFilterChain( IoSession session )
     {
         super( session );
     }
     
+    public void start()
+    {
+        flushEnabled = true;
+        flushEvents();
+    }
+    
     private void pushEvent( Event e )
     {
         eventQueue.offer( e );
-        flushEvents();
+        if( flushEnabled )
+        {
+            flushEvents();
+        }
     }
     
     private void flushEvents()
