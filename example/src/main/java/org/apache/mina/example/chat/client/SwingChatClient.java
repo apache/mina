dca273ddb0b123e7c6f0fcf94c0540a2133124b7
Peter A Royal Jr <proyal@apache.org>
2007-04-21 20:06:05 +0000

    Fix example so it connects properly
    git-svn-id: https://svn.apache.org/repos/asf/mina/branches/1.1@531099 13f79535-47bb-0310-9956-ffa450edef68

--- example/src/main/java/org/apache/mina/example/chat/client/SwingChatClient.java
+++ example/src/main/java/org/apache/mina/example/chat/client/SwingChatClient.java
@@ -6,16 +6,16 @@
  *  to you under the Apache License, Version 2.0 (the
  *  "License"); you may not use this file except in compliance
  *  with the License.  You may obtain a copy of the License at
- *  
+ *
  *    http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  *  Unless required by applicable law or agreed to in writing,
  *  software distributed under the License is distributed on an
  *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  *  KIND, either express or implied.  See the License for the
  *  specific language governing permissions and limitations
- *  under the License. 
- *  
+ *  under the License.
+ *
  */
 package org.apache.mina.example.chat.client;
 
@@ -45,7 +45,7 @@ import org.apache.mina.transport.socket.nio.SocketConnector;
 
 /**
  * Simple chat client based on Swing & MINA that implements the chat protocol.
- *  
+ *
  * @author The Apache Directory Project (mina-dev@directory.apache.org)
  * @version $Rev$, $Date$
  */
@@ -199,7 +199,7 @@ public class SwingChatClient extends JFrame implements Callback
     private class BroadcastAction extends AbstractAction
     {
         /**
-         * 
+         *
          */
         private static final long serialVersionUID = -6276019615521905411L;
 
@@ -280,7 +280,6 @@ public class SwingChatClient extends JFrame implements Callback
 
     public void connected()
     {
-        client.login();
     }
 
     public void disconnected()
