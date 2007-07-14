d5e34bb0fd3deca4db0e987421f2582d4e70f52e
Mike Heath <mheath@apache.org>
2007-07-14 03:29:23 +0000

    Removed unused imports.
    git-svn-id: https://svn.apache.org/repos/asf/mina/trunk@556214 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/mina/common/support/BaseIoSession.java
+++ core/src/main/java/org/apache/mina/common/support/BaseIoSession.java
@@ -25,8 +25,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 
 import org.apache.mina.common.ByteBuffer;
 import org.apache.mina.common.CloseFuture;
