7afce4ec7f7433a6c2ff35cc7edaaf1257a484db
Trustin Lee <trustin@apache.org>
2007-08-03 06:10:51 +0000

    Removed unnecessary synchronization in SocketConnector
    git-svn-id: https://svn.apache.org/repos/asf/mina/branches/1.1@562349 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/mina/transport/socket/nio/SocketConnector.java
+++ core/src/main/java/org/apache/mina/transport/socket/nio/SocketConnector.java
@@ -185,18 +185,16 @@ public class SocketConnector extends BaseIoConnector {
         }
 
         ConnectionRequest request = new ConnectionRequest(ch, handler, config);
-        synchronized (lock) {
+        try {
+            startupWorker();
+        } catch (IOException e) {
             try {
-                startupWorker();
-            } catch (IOException e) {
-                try {
-                    ch.close();
-                } catch (IOException e2) {
-                    ExceptionMonitor.getInstance().exceptionCaught(e2);
-                }
-
-                return DefaultConnectFuture.newFailedFuture(e);
+                ch.close();
+            } catch (IOException e2) {
+                ExceptionMonitor.getInstance().exceptionCaught(e2);
             }
+
+            return DefaultConnectFuture.newFailedFuture(e);
         }
 
         connectQueue.add(request);
@@ -222,11 +220,13 @@ public class SocketConnector extends BaseIoConnector {
         this.defaultConfig = defaultConfig;
     }
 
-    private synchronized void startupWorker() throws IOException {
-        if (worker == null) {
-            selector = Selector.open();
-            worker = new Worker();
-            executor.execute(new NamePreservingRunnable(worker));
+    private void startupWorker() throws IOException {
+        synchronized (lock) {
+            if (worker == null) {
+                selector = Selector.open();
+                worker = new Worker();
+                executor.execute(new NamePreservingRunnable(worker));
+            }
         }
     }
 
