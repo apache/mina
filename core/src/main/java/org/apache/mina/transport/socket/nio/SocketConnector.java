34ad2de59b87f6366db16ed221c00af9b4ba5313
Trustin Lee <trustin@apache.org>
2007-10-17 02:01:48 +0000

    Fixed possible FD leak in SocketConnector (I think it won't happen at most cases though...)
    git-svn-id: https://svn.apache.org/repos/asf/mina/branches/1.1@585325 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/mina/transport/socket/nio/SocketConnector.java
+++ core/src/main/java/org/apache/mina/transport/socket/nio/SocketConnector.java
@@ -254,6 +254,11 @@ public class SocketConnector extends BaseIoConnector {
                 ch.register(selector, SelectionKey.OP_CONNECT, req);
             } catch (IOException e) {
                 req.setException(e);
+                try {
+                    ch.close();
+                } catch (IOException e2) {
+                    ExceptionMonitor.getInstance().exceptionCaught(e2);
+                }
             }
         }
     }
