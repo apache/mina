be0a53c51c0bca4a4e72a992a7b834bea5be520b
Trustin Lee <trustin@apache.org>
2007-11-01 09:15:06 +0000

    Small optimization on capacity normalization
    git-svn-id: https://svn.apache.org/repos/asf/mina/trunk@590942 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/mina/common/SimpleBufferAllocator.java
+++ core/src/main/java/org/apache/mina/common/SimpleBufferAllocator.java
@@ -31,10 +31,6 @@ import java.nio.ByteOrder;
  * @version $Rev$, $Date$
  */
 public class SimpleBufferAllocator implements IoBufferAllocator {
-    private static final int MINIMUM_CAPACITY = 1;
-
-    public SimpleBufferAllocator() {
-    }
 
     public IoBuffer allocate(int capacity, boolean direct) {
         ByteBuffer nioBuffer;
@@ -70,10 +66,7 @@ public class SimpleBufferAllocator implements IoBufferAllocator {
 
         @Override
         protected void capacity0(int requestedCapacity) {
-            int newCapacity = MINIMUM_CAPACITY;
-            while (newCapacity < requestedCapacity) {
-                newCapacity <<= 1;
-            }
+            int newCapacity = normalizeCapacity(requestedCapacity);
 
             ByteBuffer oldBuf = this.buf;
             ByteBuffer newBuf;
@@ -87,6 +80,28 @@ public class SimpleBufferAllocator implements IoBufferAllocator {
             oldBuf.clear();
             newBuf.put(oldBuf);
             this.buf = newBuf;
+        }
+        
+        private static int normalizeCapacity(int requestedCapacity) {
+            switch (requestedCapacity) {
+            case 1 <<  0: case 1 <<  1: case 1 <<  2: case 1 <<  3: case 1 <<  4:
+            case 1 <<  5: case 1 <<  6: case 1 <<  7: case 1 <<  8: case 1 <<  9:
+            case 1 << 10: case 1 << 11: case 1 << 12: case 1 << 13: case 1 << 14:
+            case 1 << 15: case 1 << 16: case 1 << 17: case 1 << 18: case 1 << 19:
+            case 1 << 21: case 1 << 22: case 1 << 23: case 1 << 24: case 1 << 25:
+            case 1 << 26: case 1 << 27: case 1 << 28: case 1 << 29: case 1 << 30:
+            case Integer.MAX_VALUE:
+                return requestedCapacity;
+            }
+            
+            int newCapacity = 1;
+            while (newCapacity < requestedCapacity) {
+                newCapacity <<= 1;
+                if (newCapacity < 0) {
+                    return Integer.MAX_VALUE;
+                }
+            }
+            return newCapacity;
         }
 
         @Override
