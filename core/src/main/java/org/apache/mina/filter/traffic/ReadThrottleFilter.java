f2039b34dc7adaf974132707a160b2e9896c0a38
Trustin Lee <trustin@apache.org>
2007-11-11 14:46:02 +0000

    Related issue: DIRMINA-473 (ReadThrottleFilter throws IllegalStateException) * Reverted the previous change partially - the counter is reset when it goes negative.
    git-svn-id: https://svn.apache.org/repos/asf/mina/trunk@593896 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/mina/filter/traffic/ReadThrottleFilter.java
+++ core/src/main/java/org/apache/mina/filter/traffic/ReadThrottleFilter.java
@@ -100,6 +100,10 @@ public class ReadThrottleFilter extends IoFilterAdapter {
         return serviceBufferSize.addAndGet(increment);
     }
     
+    private static void resetServiceBufferSize(IoService service) {
+        serviceBufferSizes.remove(service);
+    }
+    
     private final AttributeKey STATE =
         new AttributeKey(ReadThrottleFilter.class, "state");
 
@@ -426,7 +430,14 @@ public class ReadThrottleFilter extends IoFilterAdapter {
         Logger logger = IoSessionLogger.getLogger(session, getClass());
 
         int globalBufferSize = ReadThrottleFilter.globalBufferSize.addAndGet(-size);
+        if (globalBufferSize < 0) {
+            ReadThrottleFilter.globalBufferSize.set(0);
+        }
+        
         int serviceBufferSize = increaseServiceBufferSize(session.getService(), -size);
+        if (serviceBufferSize < 0) {
+            resetServiceBufferSize(session.getService());
+        }
 
         int maxGlobalBufferSize = this.maxGlobalBufferSize;
         int maxServiceBufferSize = this.maxServiceBufferSize;
@@ -437,6 +448,9 @@ public class ReadThrottleFilter extends IoFilterAdapter {
         boolean enforcePolicy = false;
         synchronized (state) {
             sessionBufferSize = (state.sessionBufferSize -= size);
+            if (sessionBufferSize < 0) {
+                state.sessionBufferSize = sessionBufferSize = 0;
+            }
             if ((maxGlobalBufferSize == 0 || globalBufferSize < maxGlobalBufferSize) &&
                 (maxServiceBufferSize == 0 || serviceBufferSize < maxServiceBufferSize) &&
                 (maxSessionBufferSize == 0 || sessionBufferSize < maxSessionBufferSize)) {
