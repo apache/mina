57b21f0a4911342dc13b70f5d9fa71df45b79f17
Trustin Lee <trustin@apache.org>
2007-11-19 10:15:03 +0000

    Fixed compilation error in AsyncWeb
    git-svn-id: https://svn.apache.org/repos/asf/mina/trunk@596249 13f79535-47bb-0310-9956-ffa450edef68

--- filter-codec-http/src/main/java/org/apache/mina/filter/codec/http/HttpHeaderConstants.java
+++ filter-codec-http/src/main/java/org/apache/mina/filter/codec/http/HttpHeaderConstants.java
@@ -25,7 +25,7 @@ package org.apache.mina.filter.codec.http;
  * @author The Apache MINA Project (dev@mina.apache.org)
  * @version $Rev$, $Date$
  */
-class HttpHeaderConstants {
+public class HttpHeaderConstants {
 
     /**
      * The name of the "connection" header
