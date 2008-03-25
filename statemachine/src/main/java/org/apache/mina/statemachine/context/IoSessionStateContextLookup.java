78db42194ebf4d8952671af1535baf1d31fc04ab
Niklas Therning <niklas@apache.org>
2008-03-25 22:17:40 +0000

    DIRMINA-558: The session attribute name used can now be configured in the constructor. By using a custom attribute name several state machines may now be running for the same session.
    git-svn-id: https://svn.apache.org/repos/asf/mina/trunk@641048 13f79535-47bb-0310-9956-ffa450edef68

--- statemachine/src/main/java/org/apache/mina/statemachine/context/IoSessionStateContextLookup.java
+++ statemachine/src/main/java/org/apache/mina/statemachine/context/IoSessionStateContextLookup.java
@@ -30,20 +30,33 @@ import org.apache.mina.common.IoSession;
  */
 public class IoSessionStateContextLookup extends AbstractStateContextLookup {
     /**
-     * The name of the {@link IoSession} attribute used to store the
+     * The default name of the {@link IoSession} attribute used to store the
      * {@link StateContext} object.
      */
-    public static final String STATE_CONTEXT = 
+    public static final String DEFAULT_SESSION_ATTRIBUTE_NAME = 
         IoSessionStateContextLookup.class.getName() + ".stateContext";
+    
+    private final String sessionAttributeName;
     
     /**
      * Creates a new instance using a {@link DefaultStateContextFactory} to
      * create {@link StateContext} objects for new {@link IoSession}s.
      */
     public IoSessionStateContextLookup() {
-        this(new DefaultStateContextFactory());
+        this(new DefaultStateContextFactory(), DEFAULT_SESSION_ATTRIBUTE_NAME);
     }
 
+    /**
+     * Creates a new instance using a {@link DefaultStateContextFactory} to
+     * create {@link StateContext} objects for new {@link IoSession}s.
+     * 
+     * @param sessionAttributeName the name of the {@link IoSession} attribute
+     *        used to store the {@link StateContext} object.
+     */
+    public IoSessionStateContextLookup(String sessionAttributeName) {
+        this(new DefaultStateContextFactory(), sessionAttributeName);
+    }
+    
     /**
      * Creates a new instance using the specified {@link StateContextFactory} to
      * create {@link StateContext} objects for new {@link IoSession}s.
@@ -51,17 +64,30 @@ public class IoSessionStateContextLookup extends AbstractStateContextLookup {
      * @param contextFactory the {@link StateContextFactory}.
      */
     public IoSessionStateContextLookup(StateContextFactory contextFactory) {
-        super(contextFactory);
+	this(contextFactory, DEFAULT_SESSION_ATTRIBUTE_NAME);
     }
 
+    /**
+     * Creates a new instance using the specified {@link StateContextFactory} to
+     * create {@link StateContext} objects for new {@link IoSession}s.
+     * 
+     * @param contextFactory the {@link StateContextFactory}.
+     * @param sessionAttributeName the name of the {@link IoSession} attribute
+     *        used to store the {@link StateContext} object.
+     */
+    public IoSessionStateContextLookup(StateContextFactory contextFactory, String sessionAttributeName) {
+        super(contextFactory);
+	this.sessionAttributeName = sessionAttributeName;
+    }
+    
     protected StateContext lookup(Object eventArg) {
         IoSession session = (IoSession) eventArg;
-        return (StateContext) session.getAttribute(STATE_CONTEXT);
+        return (StateContext) session.getAttribute(sessionAttributeName);
     }
 
     protected void store(Object eventArg, StateContext context) {
         IoSession session = (IoSession) eventArg;
-        session.setAttribute(STATE_CONTEXT, context);
+        session.setAttribute(sessionAttributeName, context);
     }
 
     protected boolean supports(Class<?> c) {
