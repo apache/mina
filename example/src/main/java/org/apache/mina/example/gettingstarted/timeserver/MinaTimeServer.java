20251433365ba0735320dbe3ffdaf01a64a37667
Mark Webb <mwebb@apache.org>
2008-04-09 13:06:45 +0000

    Moved from sandbox to examples
    git-svn-id: https://svn.apache.org/repos/asf/mina/trunk@646320 13f79535-47bb-0310-9956-ffa450edef68

---
+++ example/src/main/java/org/apache/mina/example/gettingstarted/timeserver/MinaTimeServer.java
@@ -0,0 +1,32 @@
+package org.apache.mina.gettingstarted.timeserver;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.charset.Charset;
+
+import org.apache.mina.common.IdleStatus;
+import org.apache.mina.common.IoAcceptor;
+import org.apache.mina.filter.codec.ProtocolCodecFilter;
+import org.apache.mina.filter.codec.textline.TextLineCodecFactory;
+import org.apache.mina.filter.logging.LoggingFilter;
+import org.apache.mina.transport.socket.nio.NioSocketAcceptor;
+
+public class MinaTimeServer {
+
+    private static final int PORT = 9123;
+    
+    public static void main(String[] args) throws IOException {
+        
+        IoAcceptor acceptor = new NioSocketAcceptor();
+        
+        acceptor.getFilterChain().addLast( "logger", new LoggingFilter() );
+        acceptor.getFilterChain().addLast( "codec", new ProtocolCodecFilter( new TextLineCodecFactory( Charset.forName( "UTF-8" ))));
+   
+        acceptor.setHandler(  new TimeServerHandler() );
+
+        acceptor.getSessionConfig().setReadBufferSize( 2048 );
+        acceptor.getSessionConfig().setIdleTime( IdleStatus.BOTH_IDLE, 10 );
+        
+        acceptor.bind( new InetSocketAddress(PORT) );
+    }
+}
---
+++ example/src/main/java/org/apache/mina/example/gettingstarted/timeserver/TimeServerHandler.java
@@ -0,0 +1,36 @@
+package org.apache.mina.gettingstarted.timeserver;
+
+import java.util.Date;
+
+import org.apache.mina.common.IdleStatus;
+import org.apache.mina.common.IoHandlerAdapter;
+import org.apache.mina.common.IoSession;
+
+public class TimeServerHandler extends IoHandlerAdapter
+{
+    @Override
+    public void exceptionCaught( IoSession session, Throwable cause ) throws Exception
+    {
+        cause.printStackTrace();
+    }
+
+    @Override
+    public void messageReceived( IoSession session, Object message ) throws Exception
+    {
+        String str = message.toString();
+        if( str.trim().equalsIgnoreCase("quit") ) {
+            session.close();
+            return;
+        }
+
+        Date date = new Date();
+        session.write( date.toString() );
+        System.out.println("Message written...");
+    }
+
+    @Override
+    public void sessionIdle( IoSession session, IdleStatus status ) throws Exception
+    {
+        System.out.println( "IDLE " + session.getIdleCount( status ));
+    }
+}
