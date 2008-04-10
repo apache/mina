85d2a557c3ebea95e71f13b4fbef05c730eb74d6
Mark Webb <mwebb@apache.org>
2008-04-10 10:47:58 +0000

    added comments to the Step 1 server, and added a Step 2 server which introduces an ExecutorFilter
    git-svn-id: https://svn.apache.org/repos/asf/mina/trunk@646745 13f79535-47bb-0310-9956-ffa450edef68

--- example/src/main/java/org/apache/mina/example/imagine/step2/server/ImageServer.java
+++ example/src/main/java/org/apache/mina/example/imagine/step2/server/ImageServer.java
@@ -17,13 +17,17 @@
  *   under the License.
  *
  */
-package org.apache.mina.example.imagine.step1.server;
+package org.apache.mina.example.imagine.step2.server;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
 
+import org.apache.mina.common.DefaultIoFilterChainBuilder;
 import org.apache.mina.example.imagine.step1.codec.ImageCodecFactory;
+import org.apache.mina.example.imagine.step1.server.ImageServerIoHandler;
 import org.apache.mina.filter.codec.ProtocolCodecFilter;
+import org.apache.mina.filter.executor.ExecutorFilter;
 import org.apache.mina.transport.socket.nio.NioSocketAcceptor;
 
 /**
@@ -37,7 +41,6 @@ public class ImageServer {
     public static final int PORT = 33789;
 
     public static void main(String[] args) throws IOException {
-        
         // Create a class that handles sessions, incoming and outgoing data
         ImageServerIoHandler handler = new ImageServerIoHandler();
         
@@ -48,12 +51,20 @@ public class ImageServer {
         // outgoing raw data to ImageRequest and ImageResponse objects
         acceptor.getFilterChain().addLast("protocol", new ProtocolCodecFilter(new ImageCodecFactory(false)));
         
+        // get a reference to the filter chain from the acceptor
+        DefaultIoFilterChainBuilder filterChainBuilder = acceptor.getFilterChain();
+        
+        // add an ExecutorFilter to the filter chain.  The preferred order is to put the executor filter
+        // after any protocol filters due to the fact that protocol codecs are generally CPU-bound
+        // which is the same as I/O filters.
+        filterChainBuilder.addLast("threadPool", new ExecutorFilter(Executors.newCachedThreadPool()));
+        
         // set this NioSocketAcceptor's handler to the ImageServerHandler
         acceptor.setHandler(handler);
         
         // Bind to the specified address.  This kicks off the listening for 
         // incoming connections
         acceptor.bind(new InetSocketAddress(PORT));
-        System.out.println("Step 1 server is listenig at port " + PORT);
+        System.out.println("Step 2 server is listenig at port " + PORT);
     }
 }
