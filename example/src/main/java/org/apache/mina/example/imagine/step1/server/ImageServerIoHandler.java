43a370ae3bbeaf8558de2c1ebecc2ac65077db6f
Mark Webb <mwebb@apache.org>
2008-04-11 03:27:50 +0000

    added javadoc comments
    git-svn-id: https://svn.apache.org/repos/asf/mina/trunk@647039 13f79535-47bb-0310-9956-ffa450edef68

--- example/src/main/java/org/apache/mina/example/imagine/step1/server/ImageServerIoHandler.java
+++ example/src/main/java/org/apache/mina/example/imagine/step1/server/ImageServerIoHandler.java
@@ -46,14 +46,30 @@ public class ImageServerIoHandler extends IoHandlerAdapter {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    /**
+     * Called when the session is opened, which will come after the session created.
+     * 
+     * @see org.apache.mina.common.IoHandlerAdapter#sessionOpened(org.apache.mina.common.IoSession)
+     */
     public void sessionOpened(IoSession session) throws Exception {
         session.setAttribute(INDEX_KEY, 0);
     }
 
+    /**
+     * This method will be called whenever an exception occurs.  For this handler,
+     * the logger will generate a warning message.
+     * 
+     * @see org.apache.mina.common.IoHandlerAdapter#exceptionCaught(org.apache.mina.common.IoSession, java.lang.Throwable)
+     */
     public void exceptionCaught(IoSession session, Throwable cause) throws Exception {
         logger.warn(cause.getMessage(), cause);
     }
 
+    /**
+     * Handle incoming messages.
+     * 
+     * @see org.apache.mina.common.IoHandlerAdapter#messageReceived(org.apache.mina.common.IoSession, java.lang.Object)
+     */
     public void messageReceived(IoSession session, Object message) throws Exception {
         ImageRequest request = (ImageRequest) message;
         String text1 = generateString(session, request.getNumberOfCharacters());
@@ -64,6 +80,16 @@ public class ImageServerIoHandler extends IoHandlerAdapter {
         session.write(response);
     }
 
+    /**
+     * Create an image using the specified request and the text.  
+     *
+     * @param request
+     *  Determines the height and width of the image
+     * @param text
+     *  The text that is placed in the image
+     * @return
+     *  a BufferedImage representing the text.
+     */
     private BufferedImage createImage(ImageRequest request, String text) {
         BufferedImage image = new BufferedImage(request.getWidth(), request.getHeight(), BufferedImage.TYPE_BYTE_INDEXED);
         Graphics graphics = image.createGraphics();
@@ -76,6 +102,18 @@ public class ImageServerIoHandler extends IoHandlerAdapter {
         return image;
     }
 
+    /**
+     * Generate a string based on the 'characters' field in this class.  The
+     * characters that make up the string are based on the session 
+     * attribute "INDEX_KEY"
+     *
+     * @param session
+     *  The {@link IoSession} object that will provide the INDEX_KEY attribute
+     * @param length
+     *  The length that the String will be
+     * @return
+     *  The generated String
+     */
     private String generateString(IoSession session, int length) {
         Integer index = (Integer) session.getAttribute(INDEX_KEY);
         StringBuffer buffer = new StringBuffer(length);
