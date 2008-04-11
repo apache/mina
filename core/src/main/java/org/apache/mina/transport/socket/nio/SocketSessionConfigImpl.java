a5b6aa6bbe8b9b2d7e9aa29a56bb45ac613b29e5
Trustin Lee <trustin@apache.org>
2008-04-11 05:40:04 +0000

    Fixed issue: DIRMINA-560 - SocketSessionConfigImpl IPv4 localhost TEST_ADDRESS causes server to hang * Commented out localhost tests
    git-svn-id: https://svn.apache.org/repos/asf/mina/branches/1.0@647053 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/mina/transport/socket/nio/SocketSessionConfigImpl.java
+++ core/src/main/java/org/apache/mina/transport/socket/nio/SocketSessionConfigImpl.java
@@ -6,16 +6,16 @@
  *  to you under the Apache License, Version 2.0 (the
  *  "License"); you may not use this file except in compliance
  *  with the License.  You may obtain a copy of the License at
- *  
+ *
  *    http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  *  Unless required by applicable law or agreed to in writing,
  *  software distributed under the License is distributed on an
  *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  *  KIND, either express or implied.  See the License for the
  *  specific language governing permissions and limitations
- *  under the License. 
- *  
+ *  under the License.
+ *
  */
 package org.apache.mina.transport.socket.nio;
 
@@ -103,17 +103,20 @@ public class SocketSessionConfigImpl extends BaseIoSessionConfig implements
 
     private static void initializeTestAddresses() {
         try {
+            // These two tests were disabled due to DIRMINA-560
+            // (IPv4 localhost TEST_ADDRESS causes server to hang)
+
             // IPv6 localhost
-            TEST_ADDRESSES.put(new InetSocketAddress(InetAddress
-                    .getByAddress(new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                            0, 0, 0, 0, 1 }), 0), InetAddress
-                    .getByAddress(new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                            0, 0, 0, 0, 1 }));
+            //TEST_ADDRESSES.put(new InetSocketAddress(InetAddress
+            //        .getByAddress(new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            //                0, 0, 0, 0, 1 }), 0), InetAddress
+            //        .getByAddress(new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            //                0, 0, 0, 0, 1 }));
 
             // IPv4 localhost
-            TEST_ADDRESSES.put(new InetSocketAddress(InetAddress
-                    .getByAddress(new byte[] { 127, 0, 0, 1 }), 0), InetAddress
-                    .getByAddress(new byte[] { 127, 0, 0, 1 }));
+            //TEST_ADDRESSES.put(new InetSocketAddress(InetAddress
+            //        .getByAddress(new byte[] { 127, 0, 0, 1 }), 0), InetAddress
+            //        .getByAddress(new byte[] { 127, 0, 0, 1 }));
 
             // Bind to wildcard interface and connect to IPv6 localhost
             TEST_ADDRESSES.put(new InetSocketAddress(0), InetAddress
