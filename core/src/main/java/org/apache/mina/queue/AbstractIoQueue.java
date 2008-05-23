8ade1fb26bbfbe2ae1c19bfbeb11e7c595199d20
Trustin Lee <trustin@apache.org>
2008-05-23 02:53:53 +0000

    Removed unnecessary JavaDoc tags in AbstractIoQueue
    git-svn-id: https://svn.apache.org/repos/asf/mina/branches/buffer@659380 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/mina/queue/AbstractIoQueue.java
+++ core/src/main/java/org/apache/mina/queue/AbstractIoQueue.java
@@ -38,9 +38,6 @@ public abstract class AbstractIoQueue<E> extends AbstractQueue<E> implements IoQ
 
     private volatile IoQueueListener<? super E>[] listeners = newListenerArray(0);
 
-    /**
-     * {@inheritDoc}
-     */
     public final void addListener(IoQueueListener<? super E> listener) {
         if (listener == null) {
             throw new NullPointerException("listener");
@@ -57,9 +54,6 @@ public abstract class AbstractIoQueue<E> extends AbstractQueue<E> implements IoQ
         }
     }
 
-    /**
-     * {@inheritDoc}
-     */
     public final void removeListener(IoQueueListener<? super E> listener) {
         synchronized (this) {
             int index = -1;
@@ -97,9 +91,6 @@ public abstract class AbstractIoQueue<E> extends AbstractQueue<E> implements IoQ
         }
     }
 
-    /**
-     * {@inheritDoc}
-     */
     public final boolean offer(E e) {
         if (e == null) {
             throw new NullPointerException("element");
@@ -124,9 +115,6 @@ public abstract class AbstractIoQueue<E> extends AbstractQueue<E> implements IoQ
         return true;
     }
 
-    /**
-     * {@inheritDoc}
-     */
     public final E poll() {
         E e = doPoll();
         if (e == null) {
