6a753123a345d6b3df72ba3ea5ab00e574333791
Emmanuel Lecharny <elecharny@apache.org>
2008-07-08 15:23:11 +0000

    Added the deleteOnExit() flag to temporary files in tests.
    git-svn-id: https://svn.apache.org/repos/asf/mina/trunk@674856 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/test/java/org/apache/mina/filter/stream/FileRegionWriteFilterTest.java
+++ core/src/test/java/org/apache/mina/filter/stream/FileRegionWriteFilterTest.java
@@ -44,6 +44,7 @@ public class FileRegionWriteFilterTest extends AbstractStreamWriteFilterTest<Fil
     @Override
     protected FileRegion createMessage(byte[] data) throws IOException {
         File file = File.createTempFile("mina", "unittest");
+        file.deleteOnExit();
         FileChannel channel = new RandomAccessFile(file, "rw").getChannel();
         ByteBuffer buffer = ByteBuffer.wrap(data);
         channel.write(buffer);
--- core/src/test/java/org/apache/mina/transport/AbstractFileRegionTest.java
+++ core/src/test/java/org/apache/mina/transport/AbstractFileRegionTest.java
@@ -124,6 +124,7 @@ public abstract class AbstractFileRegionTest extends TestCase {
     
     private File createLargeFile() throws IOException {
         File largeFile = File.createTempFile("mina-test", "largefile");
+        largeFile.deleteOnExit();
         FileChannel channel = new FileOutputStream(largeFile).getChannel();
         ByteBuffer buffer = createBuffer();
         channel.write(buffer);
