f64233d0e23b68c224f3cc0c72c5e17aebaa9a11
Julien Vermillard <jvermillard@apache.org>
2008-08-11 07:15:58 +0000

    fixed transport serial for matching M3
    git-svn-id: https://svn.apache.org/repos/asf/mina/trunk@684652 13f79535-47bb-0310-9956-ffa450edef68

--- transport-serial/src/main/java/org/apache/mina/transport/serial/SerialSessionImpl.java
+++ transport-serial/src/main/java/org/apache/mina/transport/serial/SerialSessionImpl.java
@@ -31,7 +31,7 @@ import java.util.TooManyListenersException;
 import org.apache.mina.core.session.AbstractIoSession;
 import org.apache.mina.core.filterchain.DefaultIoFilterChain;
 import org.apache.mina.core.service.DefaultTransportMetadata;
-import org.apache.mina.core.ExceptionMonitor;
+import org.apache.mina.util.ExceptionMonitor;
 import org.apache.mina.core.buffer.IoBuffer;
 import org.apache.mina.core.filterchain.IoFilterChain;
 import org.apache.mina.core.service.IoHandler;
