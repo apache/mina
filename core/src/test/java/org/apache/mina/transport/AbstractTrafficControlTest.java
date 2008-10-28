28a8c22fea37be56180492a52edbb6f643780b6b
Emmanuel Lecharny <elecharny@apache.org>
2008-10-28 23:58:17 +0000

    Switched to Junit 4 construct, adding @Test, @Before, @After and static asserts elements in the test
    git-svn-id: https://svn.apache.org/repos/asf/mina/trunk@708731 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/test/java/org/apache/mina/transport/AbstractTrafficControlTest.java
+++ core/src/test/java/org/apache/mina/transport/AbstractTrafficControlTest.java
@@ -21,8 +21,6 @@ package org.apache.mina.transport;
 
 import java.net.SocketAddress;
 
-import junit.framework.TestCase;
-
 import org.apache.mina.core.buffer.IoBuffer;
 import org.apache.mina.core.future.ConnectFuture;
 import org.apache.mina.core.service.IoAcceptor;
@@ -30,6 +28,13 @@ import org.apache.mina.core.service.IoHandler;
 import org.apache.mina.core.service.IoHandlerAdapter;
 import org.apache.mina.core.service.TransportMetadata;
 import org.apache.mina.core.session.IoSession;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Abstract base class for testing suspending and resuming reads and
@@ -38,7 +43,7 @@ import org.apache.mina.core.session.IoSession;
  * @author The Apache MINA Project (dev@mina.apache.org)
  * @version $Rev$, $Date$
  */
-public abstract class AbstractTrafficControlTest extends TestCase {
+public abstract class AbstractTrafficControlTest {
 
     protected int port;
     protected IoAcceptor acceptor;
@@ -48,18 +53,15 @@ public abstract class AbstractTrafficControlTest extends TestCase {
         this.acceptor = acceptor;
     }
 
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
-
+    @Before
+    public void setUp() throws Exception {
         acceptor.setHandler(new ServerIoHandler());
         acceptor.bind(createServerSocketAddress(0));
         port = getPort(acceptor.getLocalAddress());
     }
 
-    @Override
-    protected void tearDown() throws Exception {
-        super.tearDown();
+    @After
+    public void tearDown() throws Exception {
         acceptor.unbind();
         acceptor.dispose();
     }
@@ -70,6 +72,7 @@ public abstract class AbstractTrafficControlTest extends TestCase {
     protected abstract SocketAddress createServerSocketAddress(int port);
     protected abstract int getPort(SocketAddress address);
 
+    @Test
     public void testSuspendResumeReadWrite() throws Exception {
         ConnectFuture future = connect(port, new ClientIoHandler());
         future.awaitUninterruptibly();
@@ -80,7 +83,7 @@ public abstract class AbstractTrafficControlTest extends TestCase {
         while (session.getAttribute("lock") == null) {
             Thread.yield();
         }
-
+        
         Object lock = session.getAttribute("lock");
         synchronized (lock) {
 
