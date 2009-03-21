6c7ad9c34415531d6bb1e27ed1b06fe7c93fb4a3
Emmanuel Lecharny <elecharny@apache.org>
2009-03-21 19:07:19 +0000

    The fix I applied for DIRMINA-672 was not sufficient : we have to get out of the loop if the decode method returns false.
    git-svn-id: https://svn.apache.org/repos/asf/mina/trunk@756989 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/mina/filter/codec/CumulativeProtocolDecoder.java
+++ core/src/main/java/org/apache/mina/filter/codec/CumulativeProtocolDecoder.java
@@ -123,8 +123,10 @@ public abstract class CumulativeProtocolDecoder extends ProtocolDecoderAdapter {
     public void decode(IoSession session, IoBuffer in,
             ProtocolDecoderOutput out) throws Exception {
         if (!session.getTransportMetadata().hasFragmentation()) {
-            while (in.hasRemaining() ) {
-                doDecode(session, in, out);
+            while (in.hasRemaining()) {
+                if (!doDecode(session, in, out)) {
+                    break;
+                }
             }
 
             return;
