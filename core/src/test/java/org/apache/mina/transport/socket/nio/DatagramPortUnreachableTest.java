a91bf8a9c289a368090d0b3250fda6ddce7e0020
Edouard De Oliveira <edeoliveira@apache.org>
2009-06-15 20:05:30 +0000

    Now uses random available port and uses lock to make the test more predictable
    git-svn-id: https://svn.apache.org/repos/asf/mina/trunk@784957 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/test/java/org/apache/mina/transport/socket/nio/DatagramPortUnreachableTest.java
+++ core/src/test/java/org/apache/mina/transport/socket/nio/DatagramPortUnreachableTest.java
@@ -20,17 +20,16 @@
 package org.apache.mina.transport.socket.nio;
 
 import java.net.InetSocketAddress;
+import java.net.PortUnreachableException;
 
 import junit.framework.TestCase;
 
 import org.apache.mina.core.buffer.IoBuffer;
 import org.apache.mina.core.future.ConnectFuture;
-import org.apache.mina.core.future.ReadFuture;
 import org.apache.mina.core.service.IoConnector;
 import org.apache.mina.core.service.IoHandlerAdapter;
 import org.apache.mina.core.session.IoSession;
 import org.apache.mina.transport.socket.DatagramSessionConfig;
-import org.apache.mina.transport.socket.nio.NioDatagramConnector;
 import org.apache.mina.util.AvailablePortFinder;
 
 /**
@@ -40,13 +39,25 @@ import org.apache.mina.util.AvailablePortFinder;
  */
 public class DatagramPortUnreachableTest extends TestCase {
 
-    private final static InetSocketAddress FAKE_ADDR = new InetSocketAddress(
-            "localhost", AvailablePortFinder.getNextAvailable(1025));
-
+    Object mutex = new Object();
+    
     private void runTest(boolean closeOnPortUnreachable) throws Exception {
         IoConnector connector = new NioDatagramConnector();
-        connector.setHandler(new IoHandlerAdapter());
-        ConnectFuture future = connector.connect(FAKE_ADDR);
+        connector.setHandler(new IoHandlerAdapter() {
+
+            @Override
+            public void exceptionCaught(IoSession session, Throwable cause)
+                    throws Exception {
+                if (cause instanceof PortUnreachableException) {
+                    synchronized(mutex) {
+                        mutex.notify();
+                    }
+                }
+            }
+            
+        });
+        ConnectFuture future = connector.connect(new InetSocketAddress("localhost", 
+                AvailablePortFinder.getNextAvailable(20000)));
         future.awaitUninterruptibly();
         IoSession session = future.getSession();
 
@@ -54,24 +65,16 @@ public class DatagramPortUnreachableTest extends TestCase {
                 .getConfig());
         cfg.setUseReadOperation(true);
         cfg.setCloseOnPortUnreachable(closeOnPortUnreachable);
-
-        session.write(IoBuffer.allocate(1)).awaitUninterruptibly().isWritten();
-        ReadFuture rf = session.read();
         
-        boolean done = false;
-        for (int i=0; i<10; i++) {
-            rf.await(1000L);
-            
-            if (session.isClosing()) {
-                assertTrue(closeOnPortUnreachable);
-                done = true;
-            }
+        synchronized(mutex) {
+            session.write(IoBuffer.allocate(1)).awaitUninterruptibly().isWritten();
+            session.read();
+            mutex.wait();
         }
         
-        if (!done) {
-            assertFalse(closeOnPortUnreachable);
-        }
-
+        Thread.sleep(500);
+        
+        assertEquals(closeOnPortUnreachable, session.isClosing());
         connector.dispose();
     }
 
@@ -79,7 +82,7 @@ public class DatagramPortUnreachableTest extends TestCase {
         // session should be closing
         runTest(true);
     }
-    
+
     public void testNormal() throws Exception {
         // test that session is not closed on port unreachable exception
         runTest(false);
