12b7986725b57187bb85634811d8ac0021c61404
Emmanuel Lecharny <elecharny@apache.org>
2010-02-10 22:03:55 +0000

    Fix for DIRMINA-762
    git-svn-id: https://svn.apache.org/repos/asf/mina/trunk@908669 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/mina/core/polling/AbstractPollingIoProcessor.java
+++ core/src/main/java/org/apache/mina/core/polling/AbstractPollingIoProcessor.java
@@ -1064,35 +1064,39 @@ public abstract class AbstractPollingIoProcessor<T extends AbstractIoSession>
                     long t1 = System.currentTimeMillis();
                     long delta = (t1 - t0);
 
-                    synchronized (wakeupCalled) {
-                        if ((selected == 0) && !wakeupCalled.get() && (delta < 100)) {
-                            // Last chance : the select() may have been
-                            // interrupted because we have had an closed channel.
-                            if (isBrokenConnection()) {
-                                // we can reselect immediately
-                                continue;
-                            } else {
-                                LOG.warn("Create a new selector. Selected is 0, delta = "
-                                                + (t1 - t0));
-                                // Ok, we are hit by the nasty epoll
-                                // spinning.
-                                // Basically, there is a race condition
-                                // which causes a closing file descriptor not to be
-                                // considered as available as a selected channel, but
-                                // it stopped the select. The next time we will
-                                // call select(), it will exit immediately for the same
-                                // reason, and do so forever, consuming 100%
-                                // CPU.
-                                // We have to destroy the selector, and
-                                // register all the socket on a new one.
-                                registerNewSelector();
-                            }
+                    if ((selected == 0) && !wakeupCalled.get() && (delta < 100)) {
+                        // Last chance : the select() may have been
+                        // interrupted because we have had an closed channel.
+                        if (isBrokenConnection()) {
+                            LOG.warn("Broken connection");
 
-                            // and continue the loop
+                            // we can reselect immediately
+                            // set back the flag to false
+                            wakeupCalled.getAndSet(false);
+
                             continue;
+                        } else {
+                            LOG.warn("Create a new selector. Selected is 0, delta = "
+                                            + (t1 - t0));
+                            // Ok, we are hit by the nasty epoll
+                            // spinning.
+                            // Basically, there is a race condition
+                            // which causes a closing file descriptor not to be
+                            // considered as available as a selected channel, but
+                            // it stopped the select. The next time we will
+                            // call select(), it will exit immediately for the same
+                            // reason, and do so forever, consuming 100%
+                            // CPU.
+                            // We have to destroy the selector, and
+                            // register all the socket on a new one.
+                            registerNewSelector();
                         }
 
+                        // Set back the flag to false
                         wakeupCalled.getAndSet(false);
+                        
+                        // and continue the loop
+                        continue;
                     }
 
                     // Manage newly created session first
