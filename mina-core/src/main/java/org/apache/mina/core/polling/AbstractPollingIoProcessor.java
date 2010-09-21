e58800e1697d5155623de85680ceab1b35e086b8
Emmanuel Lecharny <elecharny@apache.org>
2010-09-21 21:41:56 +0000

    reverted to 2.0.0-RC2 to add the missing ASL headers
    git-svn-id: https://svn.apache.org/repos/asf/mina/trunk@999658 13f79535-47bb-0310-9956-ffa450edef68

--- mina-core/src/main/java/org/apache/mina/core/polling/AbstractPollingIoProcessor.java
+++ mina-core/src/main/java/org/apache/mina/core/polling/AbstractPollingIoProcessor.java
@@ -741,6 +741,28 @@ public abstract class AbstractPollingIoProcessor<T extends AbstractIoSession>
         }
     }
 
+
+    private static String byteArrayToHex( byte[] barray )
+    {
+        char[] c = new char[barray.length * 2];
+        int pos = 0;
+
+        for ( byte b : barray )
+        {
+            int bb = ( b & 0x00FF ) >> 4;
+            c[pos++] = ( char ) ( bb > 9 ? bb + 0x37 : bb + 0x30 );
+            bb = b & 0x0F;
+            c[pos++] = ( char ) ( bb > 9 ? bb + 0x37 : bb + 0x30 );
+            if ( pos > 60 )
+            {
+                break;
+            }
+        }
+
+        return new String( c );
+    }
+
+
     private void notifyIdleSessions(long currentTime) throws Exception {
         // process idle sessions
         if (currentTime - lastIdleCheckTime >= SELECT_TIMEOUT) {
