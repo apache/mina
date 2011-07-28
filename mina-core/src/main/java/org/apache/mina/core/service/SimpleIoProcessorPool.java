e66a263f0b78c4dfe633bfd9e4ede6f20940a720
Emmanuel Lecharny <elecharny@apache.org>
2011-07-28 14:31:18 +0000

    applied the fix done in 2.0.4 tags by mistake to the branch
    git-svn-id: https://svn.apache.org/repos/asf/mina/branches/2.0.5@1151871 13f79535-47bb-0310-9956-ffa450edef68

--- mina-core/src/main/java/org/apache/mina/core/service/SimpleIoProcessorPool.java
+++ mina-core/src/main/java/org/apache/mina/core/service/SimpleIoProcessorPool.java
@@ -24,6 +24,7 @@ import java.util.Arrays;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 import org.apache.mina.core.RuntimeIoException;
 import org.apache.mina.core.session.AbstractIoSession;
@@ -157,6 +158,8 @@ public class SimpleIoProcessorPool<S extends AbstractIoSession> implements IoPro
         
         if (createdExecutor) {
             this.executor = Executors.newCachedThreadPool();
+            // Set a default reject handler
+            ((ThreadPoolExecutor)this.executor).setRejectedExecutionHandler( new ThreadPoolExecutor.CallerRunsPolicy() );
         } else {
             this.executor = executor;
         }
