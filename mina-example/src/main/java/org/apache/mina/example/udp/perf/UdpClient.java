ba39598009a9a07989b7528bcceb8e218d985fa6
Emmanuel Lecharny <elecharny@apache.org>
2012-09-27 16:37:50 +0000

    Added a performance test for UDP
    git-svn-id: https://svn.apache.org/repos/asf/mina/branches/2.0@1391091 13f79535-47bb-0310-9956-ffa450edef68

---
+++ mina-example/src/main/java/org/apache/mina/example/udp/perf/UdpClient.java
@@ -0,0 +1,150 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ */
+package org.apache.mina.example.udp.perf;
+
+import java.net.InetSocketAddress;
+
+import org.apache.mina.core.buffer.IoBuffer;
+import org.apache.mina.core.future.ConnectFuture;
+import org.apache.mina.core.service.IoConnector;
+import org.apache.mina.core.service.IoHandlerAdapter;
+import org.apache.mina.core.session.IdleStatus;
+import org.apache.mina.core.session.IoSession;
+import org.apache.mina.transport.socket.DatagramSessionConfig;
+import org.apache.mina.transport.socket.nio.NioDatagramConnector;
+
+/**
+ * An UDP client taht just send thousands of small messages to a UdpServer. 
+ * 
+ * This class is used for performance test purposes. It does nothing at all, but send a message
+ * repetitly to a server.
+ * 
+ * @author <a href="http://mina.apache.org">Apache MINA Project</a>
+ */
+public class UdpClient extends IoHandlerAdapter {
+    /** The connector */
+    private IoConnector connector;
+
+    /** The session */
+    private static IoSession session;
+
+    /**
+     * Create the UdpClient's instance
+     */
+    public UdpClient() {
+        connector = new NioDatagramConnector();
+
+        connector.setHandler(this);
+        DatagramSessionConfig dcfg = (DatagramSessionConfig) connector.getSessionConfig();
+
+        ConnectFuture connFuture = connector.connect(new InetSocketAddress("localhost", UdpServer.PORT));
+
+        connFuture.awaitUninterruptibly();
+
+        session = connFuture.getSession();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void exceptionCaught(IoSession session, Throwable cause) throws Exception {
+        cause.printStackTrace();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void messageReceived(IoSession session, Object message) throws Exception {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void messageSent(IoSession session, Object message) throws Exception {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void sessionClosed(IoSession session) throws Exception {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void sessionCreated(IoSession session) throws Exception {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void sessionIdle(IoSession session, IdleStatus status) throws Exception {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void sessionOpened(IoSession session) throws Exception {
+    }
+
+    /**
+     * The main method : instanciates a client, and send N messages. We sleep 
+     * between each K messages sent, to avoid the server saturation.
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        UdpClient client = new UdpClient();
+
+        long t0 = System.currentTimeMillis();
+
+        for (int i = 0; i <= UdpServer.MAX_RECEIVED; i++) {
+            if (i % 10 == 0) {
+                Thread.sleep(1);
+            }
+
+            String str = Integer.toString(i);
+            byte[] data = str.getBytes();
+            IoBuffer buffer = IoBuffer.allocate(data.length);
+            buffer.put(data);
+            buffer.flip();
+            session.write(buffer);
+
+            if (i % 10000 == 0) {
+                System.out.println("Sent " + i + " messages");
+            }
+        }
+
+        long t1 = System.currentTimeMillis();
+
+        System.out.println("Sent messages delay : " + (t1 - t0));
+
+        Thread.sleep(100000);
+
+        client.connector.dispose(true);
+    }
+}
