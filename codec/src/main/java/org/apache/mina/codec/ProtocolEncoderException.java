efd14146296330eb03b7ff9be24a29e0087a5e78
jvermillard <jvermillard@apache.org>
2013-06-10 23:02:39 +0200

    new exception for encoding problems

---
+++ codec/src/main/java/org/apache/mina/codec/ProtocolEncoderException.java
@@ -0,0 +1,45 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ */
+package org.apache.mina.codec;
+
+/**
+ * Exception thrown by a {@link ProtocolEncoder} if something wrong happen during encoding.
+ * 
+ * @author <a href="http://mina.apache.org">Apache MINA Project</a>
+ */
+@SuppressWarnings("serial")
+public class ProtocolEncoderException extends RuntimeException {
+
+    public ProtocolEncoderException() {
+        super();
+    }
+
+    public ProtocolEncoderException(String message) {
+        super(message);
+    }
+
+    public ProtocolEncoderException(Throwable cause) {
+        super(cause);
+    }
+
+    public ProtocolEncoderException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
