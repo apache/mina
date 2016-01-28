81b84d1f7cdc469966fed2b289e070ec34e4ff79
Emmanuel Lécharny <elecharny@symas.com>
2016-01-28 13:41:38 +0100

    Added a public method that initiates the SSL Handshake if the autoStart flag is set to false.

--- mina-core/src/main/java/org/apache/mina/filter/ssl/SslFilter.java
+++ mina-core/src/main/java/org/apache/mina/filter/ssl/SslFilter.java
@@ -30,6 +30,7 @@ import javax.net.ssl.SSLHandshakeException;
 import javax.net.ssl.SSLSession;
 
 import org.apache.mina.core.buffer.IoBuffer;
+import org.apache.mina.core.filterchain.IoFilter;
 import org.apache.mina.core.filterchain.IoFilterAdapter;
 import org.apache.mina.core.filterchain.IoFilterChain;
 import org.apache.mina.core.future.DefaultWriteFuture;
@@ -689,6 +690,29 @@ public class SslFilter extends IoFilterAdapter {
                 nextFilter.filterClose(session);
             }
         }
+    }
+
+    /**
+     * Initiate the SSL handshake. This can be invoked if you have set the 'autoStart' to
+     * false when creating the SslFilter instance.
+     * 
+     * @param session The session for which the SSL handshake should be done
+     * @throws SSLException If the handshake failed
+     */
+    public void initiateHandshake(IoSession session) throws SSLException {
+        IoFilterChain filterChain = session.getFilterChain();
+        
+        if (filterChain == null) {
+            throw new SSLException("No filter chain");
+        }
+        
+        IoFilter.NextFilter nextFilter = filterChain.getNextFilter(SslFilter.class);
+        
+        if (nextFilter == null) {
+            throw new SSLException("No SSL next filter in the chain");
+        }
+        
+        initiateHandshake(nextFilter, session);
     }
 
     private void initiateHandshake(NextFilter nextFilter, IoSession session) throws SSLException {
