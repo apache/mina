64bbf33943d3aa17dbeffaabd6f8487dcd85a50f
Emmanuel Lécharny <elecharny@symas.com>
2016-08-16 18:34:29 +0200

    Use dteh ServerSocket(0).getPort() to get an ephemeral port

--- mina-core/src/test/java/org/apache/mina/transport/socket/nio/DIRMINA777Test.java
+++ mina-core/src/test/java/org/apache/mina/transport/socket/nio/DIRMINA777Test.java
@@ -21,7 +21,9 @@ package org.apache.mina.transport.socket.nio;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.net.ServerSocket;
 
 import org.apache.mina.core.buffer.IoBuffer;
 import org.apache.mina.core.future.ConnectFuture;
@@ -29,7 +31,6 @@ import org.apache.mina.core.future.ReadFuture;
 import org.apache.mina.core.service.IoConnector;
 import org.apache.mina.core.service.IoHandlerAdapter;
 import org.apache.mina.core.session.IoSession;
-import org.apache.mina.util.AvailablePortFinder;
 import org.junit.Test;
 
 /**
@@ -41,7 +42,17 @@ public class DIRMINA777Test {
 
     @Test
     public void checkReadFuture() throws Throwable {
-        int port = AvailablePortFinder.getNextAvailable(1025);
+        int port = 0;
+        
+        try {
+            ServerSocket serverSocket = new ServerSocket(0);
+            port = serverSocket.getLocalPort();
+            serverSocket.close();
+            
+        } catch (IOException ioe) {
+            
+        }
+
         NioSocketAcceptor acceptor = new NioSocketAcceptor();
         acceptor.setReuseAddress(true);
         acceptor.setHandler(new IoHandlerAdapter() {
@@ -55,6 +66,7 @@ public class DIRMINA777Test {
             }
             
         });
+        
         acceptor.bind(new InetSocketAddress(port));
 
         try {
@@ -62,9 +74,11 @@ public class DIRMINA777Test {
             connector.setHandler(new IoHandlerAdapter());
             ConnectFuture connectFuture = connector.connect(new InetSocketAddress("localhost", port));
             connectFuture.awaitUninterruptibly();
+            
             if (connectFuture.getException() != null) {
                 throw connectFuture.getException();
             }
+            
             connectFuture.getSession().getConfig().setUseReadOperation(true);
             ReadFuture readFuture = connectFuture.getSession().read();
             readFuture.awaitUninterruptibly();
@@ -74,10 +88,9 @@ public class DIRMINA777Test {
             IoBuffer message = (IoBuffer)readFuture.getMessage();
             assertEquals(1, message.remaining());
             assertEquals(125,message.get());
-            connectFuture.getSession().close(true);
+            connectFuture.getSession().closeNow();
         } finally {
             acceptor.dispose();
         }
     }
-
 }
