13983eb28344961ff000fcd96b74f1602acc8a5e
Emmanuel Lécharny <elecharny@symas.com>
2016-10-07 11:08:00 +0200

    Fix a NPE (DIRMINA-1047)

--- mina-core/src/main/java/org/apache/mina/core/session/AbstractIoSession.java
+++ mina-core/src/main/java/org/apache/mina/core/session/AbstractIoSession.java
@@ -366,12 +366,15 @@ public abstract class AbstractIoSession implements IoSession {
         if (writeRequestQueue != null) {
             while (!writeRequestQueue.isEmpty(this)) {
                 WriteRequest writeRequest = writeRequestQueue.poll(this);
-                WriteFuture writeFuture = writeRequest.getFuture();
                 
-                // The WriteRequest may not always have a future : The CLOSE_REQUEST
-                // and MESSAGE_SENT_REQUEST don't.
-                if (writeFuture != null) {
-                    writeFuture.setWritten();
+                if (writeRequest != null) {
+                    WriteFuture writeFuture = writeRequest.getFuture();
+                    
+                    // The WriteRequest may not always have a future : The CLOSE_REQUEST
+                    // and MESSAGE_SENT_REQUEST don't.
+                    if (writeFuture != null) {
+                        writeFuture.setWritten();
+                    }
                 }
             }
         }
