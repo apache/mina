f67ccd7a50230e9b50fb589165c92b2c011461f5
Emmanuel Lécharny <elecharny@symas.com>
2017-10-27 11:17:56 +0200

    Added a Thread Factory that creates daemon threads. This can be used when passing an Executor to services.

---
+++ mina-core/src/main/java/org/apache/mina/util/DaemonThreadFactory.java
@@ -0,0 +1,43 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ */
+package org.apache.mina.util;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+
+/**
+ * A Thread Factory that creates Daemon threads
+ * 
+ *
+ * @author <a href="http://mina.apache.org">Apache MINA Project</a>
+ */
+public class DaemonThreadFactory implements ThreadFactory {
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Thread newThread(Runnable runnable) {
+        // Create daemon threads.
+        Thread thread = Executors.defaultThreadFactory().newThread(runnable);
+        thread.setDaemon(true);
+        
+        return thread;
+    }
+}
