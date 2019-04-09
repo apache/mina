94798f729dadbdb161268b18e00a8101945247d0
emmanuel lecharny <elecharny@apache.org>
2019-04-09 10:18:09 +0200

    Removed teh destroy function that is never called

--- mina-core/src/main/java/org/apache/mina/transport/socket/nio/NioSocketSession.java
+++ mina-core/src/main/java/org/apache/mina/transport/socket/nio/NioSocketSession.java
@@ -19,12 +19,9 @@
  */
 package org.apache.mina.transport.socket.nio;
 
-import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.SocketException;
-import java.nio.channels.ByteChannel;
-import java.nio.channels.SelectionKey;
 import java.nio.channels.SocketChannel;
 
 import org.apache.mina.core.RuntimeIoException;
@@ -125,15 +122,6 @@ class NioSocketSession extends NioSession {
         }
 
         return (InetSocketAddress) socket.getLocalSocketAddress();
-    }
-
-    protected void destroy(NioSession session) throws IOException {
-        ByteChannel ch = session.getChannel();
-        SelectionKey key = session.getSelectionKey();
-        if (key != null) {
-            key.cancel();
-        }
-        ch.close();
     }
 
     @Override
