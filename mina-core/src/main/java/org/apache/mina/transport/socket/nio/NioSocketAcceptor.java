c9562f8220427a53cf33ffb6dfde02f94d162b02
emmanuel lecharny <elecharny@apache.org>
2024-12-19 11:48:19 +0100

    Fixed some links; Reverted to maven source plugin 3.2.1; Fixed some spacing

--- mina-core/src/main/java/org/apache/mina/transport/socket/nio/NioSocketAcceptor.java
+++ mina-core/src/main/java/org/apache/mina/transport/socket/nio/NioSocketAcceptor.java
@@ -259,7 +259,7 @@ implements SocketAcceptor {
     protected ServerSocketChannel open(SocketAddress localAddress) throws Exception {
         // Creates the listening ServerSocket
 
-    SocketSessionConfig config = this.getSessionConfig();
+        SocketSessionConfig config = this.getSessionConfig();
     
         ServerSocketChannel channel = null;
 
